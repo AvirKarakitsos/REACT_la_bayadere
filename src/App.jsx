8bcc8511d8e3db3cbf8b6f55ec43322ef2acafab
Avir Karakitsos <avir.karakitsos@gmail.com>
2023-11-25 20:56:39 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Parallax from './components/Parallax.jsx'
+import './App.scss'
+
+function App() {
+  return (
+    <>
+      <main>
+        <Parallax />
+        <div className="container-2">
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut sint nihil esse vero dolores corporis? Sit sequi sapiente nulla porro excepturi voluptas laboriosam! Vitae cumque veritatis eius libero alias impedit.</p>
+        </div>
+        <div className="container-2">
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut sint nihil esse vero dolores corporis? Sit sequi sapiente nulla porro excepturi voluptas laboriosam! Vitae cumque veritatis eius libero alias impedit.</p>
+        </div>
+      </main>
+       
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Parallax.jsx
@@ -0,0 +1,28 @@
+import '../assets/styles/Parallax.scss'
+import parallax1 from "../assets/images/baya-para-1.jpg"
+import parallax2 from "../assets/images/baya-para-2.jpg"
+import { useRef } from "react"
+import { motion, useScroll, useTransform } from "framer-motion"
+
+function Parallax() {
+    const ref = useRef()
+
+    const { scrollYProgress } = useScroll({
+        ref: ref,
+        offset: ["start start", "end start"]
+    })
+
+    const backgroundDansers = useTransform(scrollYProgress, [0,1], ["0%","200%"])
+    const backgroundFloor = useTransform(scrollYProgress, [0,1], ["0%","100%"])
+    const title = useTransform(scrollYProgress, [0,1], ["0%","100%"])
+
+    return (
+        <div ref={ref} className="boxParallax">
+            <motion.h1 style={ {y: title} } className="boxParallax__title">La Bayadère</motion.h1>
+            <motion.img style={ {y: backgroundDansers} } className="boxParallax__background boxParallax__background--dansers" src={parallax1} alt="parallax-1"/>
+            <motion.img style={ {y: backgroundFloor} } className="boxParallax__background boxParallax__background--floor" src={parallax2} alt="parallax-2"/>
+        </div>
+    )
+}
+
+export default Parallax
