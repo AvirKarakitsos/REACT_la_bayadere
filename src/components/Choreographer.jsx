333558daa4f7cfdf123583d0fa5a356620e63c81
Avir Karakitsos <avir.karakitsos@gmail.com>
2024-01-07 18:33:22 +0100

    feat: opacity scrolling for images

--- src/components/Choreographer.jsx
+++ src/components/Choreographer.jsx
@@ -1,23 +1,32 @@
 import petipa from '../assets/images/petitpa.jpg'
 import noureev from '../assets/images/noureev.png'
 import "../assets/styles/Choreographer.scss"
+import { useScroll, motion, useTransform } from 'framer-motion'
+import { useRef } from 'react'
 
 function Choreographer() {
+    const imageRef = useRef(null)
+    const {scrollYProgress} = useScroll({
+        target: imageRef,
+        offset: ["0.5 1", "1.33 1"]
+    })
+    const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.1, 1])
+
     return(
         <div className='container'>
         <h2>De Petipa à Noureev</h2>
         <div className="choreoContainer">
-            <figure className='choreoContainer__image'>
+            <motion.figure ref={imageRef} style={ {opacity: opacityProgress} } className='choreoContainer__image'>
                 <img src={petipa} alt="Marius Petipa"/>
                 <figcaption>Marius Petipa</figcaption>
-            </figure>
+            </motion.figure>
             <div className="choreoContainer__text">
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis cum perspiciatis aut id reiciendis odio assumenda optio? Eos animi quas distinctio hic dolore saepe repellat culpa libero omnis! Magni, a!Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut sint nihil esse vero dolores corporis? Sit sequi sapiente nulla porro excepturi voluptas laboriosam! Vitae cumque veritatis eius libero alias impedit.</p>
             </div>
-            <figure className='choreoContainer__image'>
+            <motion.figure ref={imageRef} style={ {opacity: opacityProgress} } className='choreoContainer__image'>
                 <img src={noureev} alt="Rudolph Noureev"/>
                 <figcaption>Rudolph Noureev</figcaption>
-            </figure>
+            </motion.figure>
         </div>
         </div>
     )
--- src/components/Composer.jsx
+++ src/components/Composer.jsx
@@ -1,17 +1,26 @@
 import '../assets/styles/Composer.scss'
 import minkus from '../assets/images/minkus.jpg'
+import { useRef } from 'react'
+import { motion, useScroll, useTransform } from 'framer-motion'
 
 function Composer() {
     const arr = Array.from(Array(5).keys()).slice(1)
+
+    const imageRef = useRef(null)
+    const {scrollYProgress} = useScroll({
+        target: imageRef,
+        offset: ["0.5 1", "1.33 1"]
+    })
+    const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.1, 1])
 
     return (
         <div className="container">
             <h2>Ludwig Minkus</h2>
             <div className="composerContainer">
-                <figure className="composerContainer__image">
+                <motion.figure ref={imageRef} style={ {opacity: opacityProgress} }  className="composerContainer__image">
                     <img src={minkus} alt="Ludwig Minkus"/>
                     <figcaption>Ludwig Minkus</figcaption>
-                </figure>
+                </motion.figure>
                 <div className="composerContainer__text">
                     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus velit veritatis aspernatur saepe iusto dolor repudiandae odit, necessitatibus reiciendis nobis ex suscipit hic facilis quibusdam doloremque, repellendus veniam consequatur asperiores!Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus velit veritatis aspernatur saepe iusto dolor repudiandae odit, necessitatibus reiciendis nobis ex suscipit hic facilis quibusdam doloremque, repellendus veniam consequatur asperiores!</p>
                 </div>
