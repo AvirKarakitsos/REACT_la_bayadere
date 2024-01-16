e27d1e6860f05f2db0dfd0449a56973753b60559
Avir Karakitsos <avir.karakitsos@gmail.com>
2024-01-16 15:38:12 +0100

    feat: media query mobile part 1

--- src/components/Choreographer.jsx
+++ src/components/Choreographer.jsx
@@ -2,10 +2,25 @@ import petipa from '../assets/images/petitpa.jpg'
 import noureev from '../assets/images/noureev.png'
 import "../assets/styles/Choreographer.scss"
 import { useScroll, motion, useTransform } from 'framer-motion'
-import { useRef } from 'react'
+import { useRef, useState, useEffect } from 'react'
 
 function Choreographer() {
     const imageRef = useRef(null)
+    const  [windowWidth,setWindowWidth] = useState(window.innerWidth)
+
+    useEffect(() => {
+        function handleSizeViewport() {
+            setWindowWidth(window.innerWidth)
+        }
+
+        window.addEventListener("resize",handleSizeViewport)
+
+        return() => {
+            window.removeEventListener("resize", handleSizeViewport)
+        }
+
+        
+    },[windowWidth])
    
     const {scrollYProgress} = useScroll({
         target: imageRef,
@@ -22,10 +37,18 @@ function Choreographer() {
                 <motion.img style={ {opacity: opacityProgress} } src={petipa} alt="Marius Petipa"/>
                 <figcaption>Marius Petipa</figcaption>
             </figure>
-            <div className="choreoContainer__text">
-                <p className='content'><span>Petipa </span>lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis cum perspiciatis aut id reiciendis odio assumenda optio? Eos animi quas distinctio hic dolore saepe repellat culpa libero omnis! Magni, a!Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut sint nihil esse vero dolores corporis? Sit sequi sapiente nulla porro excepturi voluptas laboriosam! Vitae cumque veritatis eius libero alias impedit.</p>
-                <p className='content'><span>Noureev </span>lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis cum perspiciatis aut id reiciendis odio assumenda optio? Eos animi quas distinctio hic dolore saepe repellat culpa libero omnis! Magni, a!Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut sint nihil esse vero dolores corporis? Sit sequi sapiente nulla porro excepturi voluptas laboriosam! Vitae cumque veritatis eius libero alias impedit.</p>
-            </div>
+            {windowWidth > 768 
+                ? <div className="choreoContainer__text">
+                    <p className='content'><span>Petipa </span>lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis cum perspiciatis aut id reiciendis odio assumenda optio? Eos animi quas distinctio hic dolore saepe repellat culpa libero omnis! Magni, a!Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut sint nihil esse vero dolores corporis? Sit sequi sapiente nulla porro excepturi voluptas laboriosam! Vitae cumque veritatis eius libero alias impedit.</p>
+                    <p className='content'><span>Noureev </span>lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis cum perspiciatis aut id reiciendis odio assumenda optio? Eos animi quas distinctio hic dolore saepe repellat culpa libero omnis! Magni, a!Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut sint nihil esse vero dolores corporis? Sit sequi sapiente nulla porro excepturi voluptas laboriosam! Vitae cumque veritatis eius libero alias impedit.</p>
+                </div>
+                : <><div className="choreoContainer__text">
+                    <p className='content'><span>Petipa </span>lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis cum perspiciatis aut id reiciendis odio assumenda optio? Eos animi quas distinctio hic dolore saepe repellat culpa libero omnis! Magni, a!Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut sint nihil esse vero dolores corporis? Sit sequi sapiente nulla porro excepturi voluptas laboriosam! Vitae cumque veritatis eius libero alias impedit.</p>
+                    </div>
+                    <div className="choreoContainer__text">
+                        <p className='content'><span>Noureev </span>lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis cum perspiciatis aut id reiciendis odio assumenda optio? Eos animi quas distinctio hic dolore saepe repellat culpa libero omnis! Magni, a!Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut sint nihil esse vero dolores corporis? Sit sequi sapiente nulla porro excepturi voluptas laboriosam! Vitae cumque veritatis eius libero alias impedit.</p>
+                    </div></>
+        }
             <figure className='choreoContainer__image choreoContainer__image--noureev'>
                 <motion.img style={ {opacity: opacityProgress2} }  src={noureev} alt="Rudolph Noureev"/>
                 <figcaption>Rudolph Noureev</figcaption>
